eacdad66f080566be17d922f8fa8ab3c6b39d984
arron <fsxchen@gmail.com>
2018-04-23 22:38:28 +0800

    增加了一些models

--- apps/trade/models.py
+++ apps/trade/models.py
@@ -1,3 +1,67 @@
 from django.db import models
-
+# from users.models import UserProfile
 # Create your models here.
+from django.contrib.auth import get_user_model
+from goods.models import Goods
+
+from datetime import datetime
+
+User = get_user_model()
+
+
+class ShoppingCart(models.Model):
+    user = models.ForeignKey(User)
+    goods = models.ForeignKey(Goods)
+    goods_num = models.IntegerField()
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "购物车"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.user.goods.name
+
+
+class OrderInfo(models.Model):
+
+    PAY_STATUS = (
+        ("success", "成功"),
+        ("cancel", "取消"),
+        ("wait", "待支付")
+    )
+
+    PAY_TYPE = (
+        ("alipay", "支付宝"),
+        ("wechat", "微信")
+    )
+    user = models.ForeignKey(User)
+    order_sn = models.CharField(max_length=11, unique=True, verbose_name="订单编号")
+    trade_no = models.CharField(max_length=13, unique=True, verbose_name="交易编号", blank=True)
+    pay_status = models.CharField(max_length=10, choices=PAY_STATUS, verbose_name="订单状态")
+    post_script = models.CharField(max_length=200, verbose_name="订单留言")
+    order_mount = models.FloatField(max_length=11, default=0.0, verbose_name="订单金额")
+    pay_time = models.DateTimeField(verbose_name="支付时间")
+
+    address = models.CharField()
+    signer_name = models.CharField(verbose_name="签收人姓名")
+    signer_mobile = models.CharField()
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+
+class OrderGoods(models.Model):
+    """
+    订单的商品详情
+    """
+    order = models.ForeignKey(OrderInfo)
+    goods = models.ForeignKey(Goods)
+    goods_num = models.IntegerField(default=0, verbose_name="商品数量")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "订单的商品详情"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.order.order_sn
+
--- apps/user_operation/models.py
+++ apps/user_operation/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class UserFav(models.Model):
+    pass
