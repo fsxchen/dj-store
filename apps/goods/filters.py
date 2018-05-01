90fd405302aa990af0ec305bed327e241a9c0d92
arron <fsxchen@gmaill.com>
2018-05-01 21:41:14 +0800

    filter

--- apps/goods/filters.py
+++ apps/goods/filters.py
@@ -1,12 +1,19 @@
 import django_filters
 from .models import Goods
+from django.db.models import Q
 
 class ProductFilter(django_filters.FilterSet):
-    price_min = django_filters.NumberFilter(name="shop_price", lookup_expr="gte")
-    price_max = django_filters.NumberFilter(name="shop_price", lookup_expr='lte')
+    pricemin = django_filters.NumberFilter(name="shop_price", lookup_expr="gte")
+    pricemax = django_filters.NumberFilter(name="shop_price", lookup_expr='lte')
     # 不加lookup，就精确匹配
     name = django_filters.CharFilter(name="name", lookup_expr='icontains')
+    top_category = django_filters.NumberFilter(method='top_category_filter')
+
+    def top_category_filter(self, queryset, name, value):
+        return queryset.filter(Q(category_id=value) | Q(category__parent_category_id=value) | Q(
+            category__parent_category__parent_category_id=value))
+
 
     class Meta:
         model = Goods
-        fields = ["price_min", "price_max", 'name']
+        fields = ["pricemin", "pricemax", 'name']
